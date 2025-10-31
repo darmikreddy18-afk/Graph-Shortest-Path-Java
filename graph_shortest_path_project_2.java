7aaf99e2ed9eb53d42d6dba85bb87ccf6a31c0f2
darmikreddy18-afk <darmikreddy18@gmail.com>
2025-10-31 12:37:09 +0530

    Added Bellman-Ford algorithm and detailed README for both algorithms
    Added a new file (graph_shortest_path_project_2.java) implementing the Bellman-Ford algorithm.
    - The algorithm supports both positive and negative edge weights.
    - Included a sample test graph and expected output in the README.
    - Updated README.md with detailed sections for Dijkstra’s and Bellman-Ford algorithms.
    - Added example graphs, how to run instructions, and comparison table between both algorithms.

---
+++ graph_shortest_path_project_2.java
@@ -0,0 +1,62 @@
+import java.util.*;
+class graph_shortest_path_project_2{
+    static class Edge{
+        int src;
+        int dest;
+        int wt;
+    
+    Edge(int src,int dest,int wt){
+        this.src=src;
+        this.dest=dest;
+        this.wt=wt;
+    }
+}
+public static void graph_insert(ArrayList<Edge> graph[]){
+        for(int i=0;i<graph.length;i++){
+            graph[i]=new ArrayList<Edge>();
+        }
+        graph[0].add(new Edge(0, 1, 4));
+        graph[0].add(new Edge(0, 2, 5));
+        graph[1].add(new Edge(1, 2, -3));
+        graph[1].add(new Edge(1, 3, 6));
+        graph[2].add(new Edge(2, 3, 2));
+        graph[3].add(new Edge(3, 4, -1));
+        
+
+        
+
+
+    }
+public static void Bellman_Ford(ArrayList<Edge> graph[],int src,int V){
+    int dist[]=new int[V];
+    for(int i=0;i<V;i++){
+        if(i!=src){
+            dist[i]=Integer.MAX_VALUE;
+        }
+    }
+    for(int k=0;k<V-1;k++){
+        for(int i=0;i<V;i++){
+            for(int j=0;j<graph[i].size();j++){
+                Edge e=graph[i].get(j);
+                int u=e.src;
+                int v=e.dest;
+                if(dist[u]!=Integer.MAX_VALUE && dist[u]+e.wt<dist[v]){
+                    dist[v]=dist[u]+e.wt;
+                }
+            }
+
+        }
+    }
+    for(int i=0;i<V;i++){
+        System.out.println("the distance of vertex "+i+" from the source "+src+" is "+dist[i]);
+    }
+}
+public static void main(String[] args) {
+    ArrayList<Edge> graph[]=new ArrayList[5];
+    graph_insert(graph);
+    Bellman_Ford(graph, 0, 5);
+
+
+}
+
+}
